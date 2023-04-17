ee227b044500ac0bd08b9c9437bc87484e4dc40a
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-17 14:55:32 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [message, setMessage] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(dogs => setMessage(dogs.message));
+    }, []);
+
+    if (!message) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={message} alt="A Random Dog"/>
+        </div>
+    )
+}
+
+export default App;
